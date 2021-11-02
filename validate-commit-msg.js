1bd42405f2dc65227b32e65b8d5600f9b8c5a35d
‘郭海杰’ <ghj01141776@alibaba-inc.com>
2021-11-02 16:47:34 +0800

    1

--- validate-commit-msg.js
+++ validate-commit-msg.js
@@ -1,15 +1,1 @@
-module.exports = {
-    "types": ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"],
-    "scope": {
-      "required": false,
-      "allowed": ["*"],
-      "validate": false,
-      "multiple": false
-    },
-    "warnOnFail": false,
-    "maxSubjectLength": 100,
-    "subjectPattern": ".+",
-    "subjectPatternErrorMsg": "subject does not match subject pattern!",
-    "helpMessage": "",
-    "autoFix": false
-}
+module.exports = require('./lib/validateMessage').validateMessage;
